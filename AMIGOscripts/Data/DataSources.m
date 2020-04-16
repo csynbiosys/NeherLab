c965ce549a112800639f9de5ce9b7fe469bf401c
DavidGomezC <38907407+DavidGomezC@users.noreply.github.com>
2020-04-16 12:41:56 +0000

    Section of the repository containing data from Italy, Spain and UK
    DataParser_Italy.m automatically extract the data from the selected source. Run this code to obtain updated data (remember to change the date in the name of the saved csv file)

---
+++ AMIGOscripts/Data/DataSources.m
@@ -0,0 +1,17 @@
+%This script contains the URL for data fetchnig. 
+
+
+URL_Italy= 'https://raw.github.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json',
+% "dataProvenance": "Dipartimento della Protezione Civile",
+% "license": "CC-BY-4.0"
+% Note: link retrieved from NeherLab sources
+
+URL_Spain = 'https://github.com/datadista/datasets/tree/master/COVID%2019';
+% "dataProvenance": "http://www.datadista.com",
+% "license": "unclear"
+% Note: link retrieved from NeherLab sources
+% Check alternative url provided by Eva (https://covid19.isciii.es/), where
+% it seems you need to click in order to download data
+
+URL_UK = 
+ 
