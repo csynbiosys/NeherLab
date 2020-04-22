3db50dc7579979fa0492701f628ac5a66e7e8f6d
Freddie Starkey <freddie.starkey@gmail.com>
2020-04-22 12:37:31 +0100

    Delete plots.py

--- plots.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_result(name, title, x_label, y_label, time, time_series):
-  plt.figure()
-  plt.title(title)
-  plt.xlabel(x_label)
-  plt.ylabel(y_label)
-  plt.plot(time, time_series)
-  plt.savefig(name)
